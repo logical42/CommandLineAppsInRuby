3c892e65ee314b78b8102ab0aec202d8a515d16a
Han <hkfortytwo@gmail.com>
2012-11-13 17:10:02 -0800

    rspec tests for db_backup

--- db_backup/spec/options_spec.rb
+++ db_backup/spec/options_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 describe "options tests" do
   it 'should store the correct user option' do
-    `bin/db_backup -u asdf`.should match(/./)
+    `bin/db_backup -t -u asdf`.should match(/username=asdf/)
+  end
+  it 'should store the correct password option' do
+    `bin/db_backup -t -p password`.should match(/password=password/)
   end
 end
--- db_backup/spec/spec_helper.rb
+++ db_backup/spec/spec_helper.rb
@@ -75,7 +75,9 @@ require 'rspec/autorun'
 #require 'factory_girl'
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
-
+# def RSpec.env
+#   return "test"
+# end
 RSpec.configure do |config|
   # ## Mock FrameworkRSpec.configure do |config|
   #@@config.include FactoryGirl::Syntax::Methods
@@ -100,5 +102,4 @@ RSpec.configure do |config|
   # rspec-rails.
   #config.infer_base_class_for_anonymous_controllers = false
 end
-
 
