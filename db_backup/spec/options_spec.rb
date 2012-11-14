966a0153f6fd6952081dc9b01e4b021dc1a3b597
Han <hkfortytwo@gmail.com>
2012-11-13 18:21:11 -0800

    guard file

--- db_backup/spec/options_spec.rb
+++ db_backup/spec/options_spec.rb
@@ -1,9 +1,14 @@
 require 'spec_helper'
 describe "options tests" do
   it 'should store the correct user option' do
-    `bin/db_backup -t -u asdf`.should match(/username=asdf/)
+    `bin/db_backup -t -u asdf asdfasdfa`.should match(/username=asdf/)
   end
   it 'should store the correct password option' do
-    `bin/db_backup -t -p password`.should match(/password=password/)
+    `bin/db_backup -t -p password asdfasdf`.should match(/password=password/)
+  end
+end
+describe "a" do
+  it 'asdfasdf' do
+    true
   end
 end
